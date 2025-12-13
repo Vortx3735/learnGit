64202349761763c703a9208ccc78d658e4be66bf
teralithtechnologies <teralithtechnologies@gmail.com>
2025-12-12 18:09:52 -0600

    Clarified print statement

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("I do not like lucas");
+    System.out.println("if everyone who liked lucas gave me a billion dollars, I'd be broke");
   }
 }
